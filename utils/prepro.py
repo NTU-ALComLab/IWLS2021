190f109430e9b16f48c7d036670c5ce3d584c819
b04901112 <b04901112@ntu.edu.tw>
2021-04-21 15:10:02 +0800

    add img prepro methods peel/bitblast/rmLSB

--- utils/prepro.py
+++ utils/prepro.py
@@ -1,5 +1,49 @@
 import numpy as np
+# data shape: (n_data, n_channel, img_length, img_width)
 
-# peel: remove the outermost layer of the image
-# sample: down-sample the pixels with the specified stride
-# 
+# remove the outermost n layers of the image
+def imgPeel(data, n=1):
+    assert isinstance(n, int)
+    ret = []
+    for dat in data:
+        img = []
+        for ch in dat:
+            if 2*n > len(ch):
+                print('Cannot peel {} layers of a {}x{} image.'.format(str(n), str(len(ch)), str(len(ch))))
+                return data
+            for x in ch[n:-n]:
+                img.append(x[n:-n])
+        ret.append(img)
+    return np.array(ret, dtype=np.uint8)
+
+# down-sample the pixels with the specified stride
+# case: stride = 0.5
+#       10101010...
+#       01010101...
+# case: stride = 1
+#       10101010...
+#       00000000...
+#       10101010...
+def imgDownSample(data, n=0.5):
+    pass
+
+# merge channels
+# flag: specify which channel is used
+def imgMergeChannel(data, flag=(True, True, True)):
+    assert len(flag) == 3
+    pass
+
+# remove n LSBs
+def imgRemoveLSB(data, n=1):
+    assert isinstance(n, int) and (n >=0) and (n < 8)
+    return data >> n
+
+
+# bit blast the image vectors
+# n: number of bits
+def imgBitBlast(data, n=8):
+    assert isinstance(n, int) and (n >=0) and (n <= 8)
+    ret = []
+    for i in range(n):
+        ret.append((data >> n) & 1)
+    return np.stack(ret,axis=-1)
--- utils/utils.py
+++ utils/utils.py
@@ -10,3 +10,20 @@ def pkLoad(fn):
 
 
 # compute accuracy according to IWLS 2021 rule
+
+# get the label of a 1-hot vector
+# each element in the vector should be within the range [0, 1]
+def getPredLabel(v):
+    assert len(v) == 10  # cifar10 class
+    for i in range(10):
+        if v[i] > 0.5:
+            return i
+
+# compute accuaracy
+def getAcc(preds, labels):
+    assert len(preds) == len(labels)
+    cnt = 0
+    for i, j in zip(preds, labels):
+        if getPredLabel(i) == j:
+            cnt += 1
+    return cnt / len(preds)
