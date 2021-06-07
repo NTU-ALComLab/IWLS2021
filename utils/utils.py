91cb013320925ae44f60c9adcdd443d695827927
b04901112 <b04901112@ntu.edu.tw>
2021-06-07 11:55:25 +0800

    remove used code from utils

--- utils/utils.py
+++ utils/utils.py
@@ -7,23 +7,3 @@ def pkDump(x, fn):
 
 def pkLoad(fn):
     return pk.load(open(fn, 'rb'))
-
-
-# compute accuracy according to IWLS 2021 rule
-
-# get the label of a 1-hot vector
-# each element in the vector should be within the range [0, 1]
-def getPredLabel(v):
-    assert len(v) == 10  # cifar10 class
-    for i in range(10):
-        if v[i] > 0.5:
-            return i
-
-# compute accuaracy
-def getAcc(preds, labels):
-    assert len(preds) == len(labels)
-    cnt = 0
-    for i, j in zip(preds, labels):
-        if getPredLabel(i) == j:
-            cnt += 1
-    return cnt / len(preds)
