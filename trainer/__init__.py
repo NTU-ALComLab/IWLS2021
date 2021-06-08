9587ef010f30839f536ee96bc9fa8a68ce2c2730
b04901112 <b04901112@ntu.edu.tw>
2021-06-08 12:17:59 +0800

    add randomforest training method

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -1,14 +1,23 @@
 from .ClfTrainer import Trainer
 from .DTree import DTree
+from .RForest import RForest
 
 dtParams = {
     'criterion': 'entropy',
     #'max_depth': 30,
     'class_weight': 'balanced',
-    #'ccp_alpha': 0.015
+    #'ccp_alpha': 0.015,
 }
 
-#rfParams = {}
+rfParams = {
+    'n_estimators': 100,
+    'criterion': 'entropy',
+    'max_depth': 30,
+    'n_jobs': 1,
+    'class_weight': 'balanced_subsample',
+    #'ccp_alpha': 0.015,
+}
+
 #lutParams = {}
 
 def getTrainer(clfType='dt', nClass=10, mode='dir', verbose=True, clfParams=None):
@@ -16,7 +25,8 @@ def getTrainer(clfType='dt', nClass=10, mode='dir', verbose=True, clfParams=None
         params = dtParams if (clfParams is None) else clfParams
         clf = DTree
     elif clfType == 'rf':
-        raise NotImplementedError()
+        params = rfParams if (clfParams is None) else clfParams
+        clf = RForest
     elif clfType == 'lut':
         raise NotImplementedError()
     else:
