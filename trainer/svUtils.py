7c83b30c3389b75fb6c77eb14d40c69fd1705e26
b04901112 <b04901112@ntu.edu.tw>
2021-06-16 01:13:57 +0800

    add system verilog util functions

---
+++ trainer/svUtils.py
@@ -0,0 +1,45 @@
+# templates
+svTemplateTxt = \
+'''
+module MODULE(
+    IOPORTS
+);
+
+VARS
+
+BODY
+
+endmodule
+'''
+
+svModuleTxt = \
+"""
+MODULE MODULE_inst_IDX(IOPORTS);
+"""
+
+# generate var (io/wire) texts
+# varList: list of tuples (var_type, n_bits, port_name, array_len)
+def svVarGen(varList):
+    assert len(varList) > 0
+
+    getNB = lambda n: '' if (n == 1) else '[{}:0]'.format(str(n - 1))
+    getAL = lambda n: '' if (n == 1) else '[0:{}]'.format(str(n - 1))
+
+    ret = []
+    for x in varList:
+        assert (len(x) == 4) and (x[0] in {'input', 'output', 'wire'}) and (x[1] > 0) and (x[3] > 0)
+        ret.append('{} {} {} {};\n'.format(x[0], getNB(x[1]), x[2], getAL(x[3])))
+    
+    return ''.join(ret)
+
+# assign var = val;
+def svAssign(var, val):
+    return 'assign {} = {};\n'.format(var, val)
+
+# ioList: list of (module port, wire) tuples
+def svModule(name, idx, ioList):
+    ios = ', '.join(['.{}({})'.format(i, j) for i, j in ioList])
+    ret = svModuleTxt.replace('MODULE', name) \
+                     .replace('IDX', str(idx)) \
+                     .replace('IOPORTS', ios)
+    return ret
