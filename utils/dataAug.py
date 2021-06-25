9fdc7097b2f5b9239ddf5ea5dff8f9aa3bad8d40
b04901112 <b04901112@ntu.edu.tw>
2021-06-25 18:50:21 +0800

    reduce image distortion

--- utils/dataAug.py
+++ utils/dataAug.py
@@ -6,29 +6,29 @@ from joblib import Parallel, delayed
 albumTrans = A.Compose([
     #A.RandomRotate90(),
     A.Flip(),
-    A.Transpose(),
-    A.OneOf([
-        A.IAAAdditiveGaussianNoise(),
-        A.GaussNoise(),
-    ], p=0.2),
-    A.OneOf([
-        A.MotionBlur(p=.2),
-        A.MedianBlur(blur_limit=3, p=0.1),
-        A.Blur(blur_limit=3, p=0.1),
-    ], p=0.2),
-    A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.2),
-    A.OneOf([
-        A.OpticalDistortion(p=0.3),
-        A.GridDistortion(p=.1),
-        A.IAAPiecewiseAffine(p=0.3),
-    ], p=0.2),
-    A.OneOf([
-        A.CLAHE(clip_limit=2),
-        A.IAASharpen(),
-        A.IAAEmboss(),
-        A.RandomBrightnessContrast(),            
-    ], p=0.3),
-    A.HueSaturationValue(p=0.3),
+    #A.Transpose(),
+    #A.OneOf([
+    #    A.IAAAdditiveGaussianNoise(),
+    #    A.GaussNoise(),
+    #], p=0.2),
+    #A.OneOf([
+    #    A.MotionBlur(p=.2),
+    #    A.MedianBlur(blur_limit=3, p=0.1),
+    #    A.Blur(blur_limit=3, p=0.1),
+    #], p=0.2),
+    A.ShiftScaleRotate(shift_limit=0.05, scale_limit=0.1, rotate_limit=5, p=0.2),
+    #A.OneOf([
+    #    A.OpticalDistortion(p=0.3),
+    #    A.GridDistortion(p=.1),
+    #    A.IAAPiecewiseAffine(p=0.3),
+    #], p=0.2),
+    #A.OneOf([
+    #    A.CLAHE(clip_limit=2),
+    #    A.IAASharpen(),
+    #    A.IAAEmboss(),
+    #    A.RandomBrightnessContrast(),            
+    #], p=0.3),
+    #A.HueSaturationValue(p=0.3),
 ])
 
 def imgTransform(img):
